00b10daf5a57d4f485219327c55593346b925d55
Gautam Juvarajiya <gautamj9382@gmail.com>
2022-05-12 13:20:11 +0530

    Update otp_generator.py

--- otp_generator.py
+++ otp_generator.py
@@ -7,23 +7,25 @@ from tkinter import messagebox
 
 ws = Tk()
 ws.title('OTP generator')
-ws.geometry('350x200')
+ws.geometry('350x200')    # Setting Size of GUI Window
 
+# Generating Random OTP(6-digit code)
 digits="0123456789"
 OTP=""
 for i in range(6):
     OTP+=digits[math.floor(random.random()*10)]
-otp = OTP+ " is your Python Project Verification Code"
+otp = OTP+ " is your Python Project Verification Code"      # Message which will be displayed in Mail along with OTP
 msg= otp
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 s.login("Your Gmail Account", "Your app password")
 
-
+# When user enter mail and click on Send Mail Button this function is called
 def sendingg():
     strr=str(a.get())
     s.sendmail('&&&&&&&&&&&',strr,msg)
 
+# When user validate the OTP if it is entered, if entered matched or not, this function helps for it
 def same():
     st=str(b.get())
     if st=='':
@@ -33,7 +35,7 @@ def same():
     else:
         return messagebox.showinfo('message','Hi! Your Entered OTP is Not Matched')
 
-
+#Tkinter Widgets
 Label(ws, text="Enter Email :").pack()
 a=StringVar()
 enteredmail = Entry(ws,textvariable=a)
@@ -47,4 +49,3 @@ enteredotp.pack()
 Button(ws, text="VERIFY OTP", command=same).pack()
 
 ws.mainloop()
-
